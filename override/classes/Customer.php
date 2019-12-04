cae7fd0cc2baac6bdfa610b6a52063534bafa633
Stephan <stephan.camilleri@web-equip.fr>
2019-12-04 16:40:41 +0100

    update

--- override/classes/Customer.php
+++ override/classes/Customer.php
@@ -184,7 +184,7 @@ class Customer extends CustomerCore {
 	**/
 	public static function search($term) {
 
-		$term = "'%".pSql($term)."%'";
-		return Db::getInstance()->executeS("SELECT id_customer AS id, firstname, lastname, email, company FROM ps_customer WHERE firstname LIKE $term collate utf8_bin OR lastname LIKE $term collate utf8_bin OR email LIKE $term OR company LIKE $term collate utf8_bin");	
+		$term = "'%".pSql(strtolower($term))."%'";
+		return Db::getInstance()->executeS("SELECT id_customer AS id, firstname, lastname, email, company FROM ps_customer WHERE LOWER(firstname) LIKE $term collate utf8_bin OR LOWER(lastname) LIKE $term collate utf8_bin OR LOWER(email) LIKE $term OR LOWER(company) LIKE $term collate utf8_bin");	
 	}
 }
