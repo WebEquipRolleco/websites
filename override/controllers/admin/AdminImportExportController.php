64b9e09648a6a22f2b5801cbd591ad9ee8768d29
Stephan <stephan.camilleri@web-equip.fr>
2020-01-15 09:50:27 +0100

    update

--- override/controllers/admin/AdminImportExportController.php
+++ override/controllers/admin/AdminImportExportController.php
@@ -306,7 +306,7 @@ class AdminImportExportControllerCore extends AdminController {
                         if($row['id_supplier'] and $row["supplier_reference"]) {
 
                             $supplier = new ProductSupplier();
-                            $supplier->id_product = $row['id_product'];
+                            $supplier->id_product = $product->id;
                             $supplier->id_product_attribute = 0;
                             $supplier->id_supplier = $row['id_supplier'];
                             $supplier->product_supplier_reference = $row["supplier_reference"];
