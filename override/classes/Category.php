a4e5d2405d988001ef5d9297c69ac2d4cc6cf0b7
Stephan <stephan.camilleri@web-equip.fr>
2020-06-02 17:27:27 +0200

    correction nombre de références catégories

--- override/classes/Category.php
+++ override/classes/Category.php
@@ -20,23 +20,23 @@ class Category extends CategoryCore {
 		$nb_products = 0;
 		$nb_combinations = 0;
 
+		$ids_category[] = $this->id;
+
 		$tree = $this->recurseLiteCategTree();
 		if(isset($tree['children']))
 			foreach($tree['children'] as $child) 
 				$ids_category[] = $child['id'];
 
-		if(isset($ids_category)) {
-			$ids_category = implode(',', $ids_category);
+		$ids_category = implode(',', $ids_category);
 
-			$ids_products = Db::getInstance()->executeS("SELECT p.id_product FROM ps_product p, ps_category_product cp, ps_category c WHERE cp.id_product = p.id_product AND p.active = 1 AND cp.id_category = c.id_category AND c.active = 1 AND p.visibility <> 'none' AND cp.id_category IN ($ids_category)");
-			if($ids_products) {
+		$ids_products = Db::getInstance()->executeS("SELECT p.id_product FROM ps_product p, ps_category_product cp, ps_category c WHERE cp.id_product = p.id_product AND p.active = 1 AND cp.id_category = c.id_category AND c.active = 1 AND p.visibility <> 'none' AND cp.id_category IN ($ids_category)");
+		if($ids_products) {
 
-				$ids_products = array_map(function($e) { return $e['id_product']; }, $ids_products);
-				$ids_products = implode(',', $ids_products);
+			$ids_products = array_map(function($e) { return $e['id_product']; }, $ids_products);
+			$ids_products = implode(',', $ids_products);
 
-				$nb_products = (int)Db::getInstance()->getValue("SELECT COUNT(id_product) FROM ps_product WHERE id_product IN ($ids_products) AND id_product NOT IN (SELECT id_product FROM ps_product_attribute)");
-				$nb_combinations = (int)Db::getInstance()->getValue("SELECT COUNT(id_product_attribute) FROM ps_product_attribute WHERE id_product IN ($ids_products)");
-			}
+			$nb_products = (int)Db::getInstance()->getValue("SELECT COUNT(id_product) FROM ps_product WHERE id_product IN ($ids_products) AND id_product NOT IN (SELECT id_product FROM ps_product_attribute)");
+			$nb_combinations = (int)Db::getInstance()->getValue("SELECT COUNT(id_product_attribute) FROM ps_product_attribute WHERE id_product IN ($ids_products)");
 		}
 
 		return $nb_products + $nb_combinations;
