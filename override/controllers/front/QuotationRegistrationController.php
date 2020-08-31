f8dd3943f84d5ca78bbf76b8da6103b98260c80d
Thierry <thierry.gozdzicki@provost.fr>
2020-08-31 15:07:08 +0200

    [Front] Envoi devis par email

--- override/controllers/front/QuotationRegistrationController.php
+++ override/controllers/front/QuotationRegistrationController.php
@@ -21,8 +21,12 @@ class QuotationRegistrationControllerCore extends FrontController {
 
             $tpl_vars['{shop_name}'] = $this->context->shop->name;
 
-            Mail::send(1, "quotation_request", "Demande de devis", $tpl_vars, Configuration::get('PS_SHOP_EMAIL'), "Web-equip", $form['email'], trim($form['firstname']." ".$form['lastname']));
+
+            Mail::send(1, "quotation_request", "Demande de devis", $tpl_vars, Configuration::get('PS_SHOP_EMAIL'), "Web-equip", $form['email'], trim($form['firstname']." ".$form['lastname'])
+                , null, null, null, null, null, "thierry.gozdzicki@provost.fr");
             $this->context->smarty->assign('validation', true);
+            var_dump($tpl_vars);
+            die();
         }
 
         $this->context->smarty->assign('breadcrumb', $data);
