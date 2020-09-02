80631196a0d82d5e8d32c8d28fa1ea447aa6b047
Thierry <thierry.gozdzicki@provost.fr>
2020-09-02 10:51:12 +0200

    [BO order] Ajout javadoc

--- override/classes/order/OrderHistory.php
+++ override/classes/order/OrderHistory.php
@@ -64,6 +64,7 @@ class OrderHistory extends OrderHistoryCore {
                 LEFT JOIN `'._DB_PREFIX_.'order_state` os ON oh.`id_order_state` = os.`id_order_state`
                 LEFT JOIN `'._DB_PREFIX_.'order_state_lang` osl ON (os.`id_order_state` = osl.`id_order_state` AND osl.`id_lang` = o.`id_lang`)
             WHERE oh.`id_order_history` = '.(int)$this->id.' AND os.`send_email` = 1');
+
         if (isset($result['template']) && Validate::isEmail($result['email'])) {
             ShopUrl::cacheMainDomainForShop($order->id_shop);
 
@@ -95,12 +96,14 @@ class OrderHistory extends OrderHistoryCore {
             $data['{total_paid}'] = Tools::displayPrice((float)$order->total_paid, new Currency((int)$order->id_currency), false);
 
             if (Validate::isLoadedObject($order)) {
-                // Attach invoice and / or delivery-slip if they exists and status is set to attach them
+
+                /* Condition en cas de fichier a ajouter en tant que piece jointe */
                 if (($result['pdf_invoice'] || $result['pdf_delivery'])) {
                     $context = Context::getContext();
                     $invoice = $order->getInvoicesCollection();
                     $file_attachement = array();
 
+                    /* Condition pour la generation de la facture */
                     if ($result['pdf_invoice'] && (int)Configuration::get('PS_INVOICE') && $order->invoice_number) {
                         Hook::exec('actionPDFInvoiceRender', array('order_invoice_list' => $invoice));
                         $pdf = new PDF($invoice, PDF::TEMPLATE_INVOICE, $context->smarty);
@@ -108,26 +111,33 @@ class OrderHistory extends OrderHistoryCore {
                         $file_attachement['invoice']['name'] = Configuration::get('PS_INVOICE_PREFIX', (int)$order->id_lang, null, $order->id_shop).sprintf('%06d', $order->invoice_number).'.pdf';
                         $file_attachement['invoice']['mime'] = 'application/pdf';
                     }
+
+                    /* Condition pour la generation du bordereau de livraison */
                     if ($result['pdf_delivery'] && $order->delivery_number) {
                         $pdf = new PDF($invoice, PDF::TEMPLATE_DELIVERY_SLIP, $context->smarty);
                         $file_attachement['delivery']['content'] = $pdf->render(false);
                         $file_attachement['delivery']['name'] = Configuration::get('PS_DELIVERY_PREFIX', Context::getContext()->language->id, null, $order->id_shop).sprintf('%06d', $order->delivery_number).'.pdf';
                         $file_attachement['delivery']['mime'] = 'application/pdf';
                     }
+
+                    /* Condition pour la generation des conditions de ventes */
                     if($result['term_of_use'] and $order->getShop() and $order->getShop()->hasConditionsFile()) {
                         $file_attachement['term_of_use']['content'] = file_get_contents($order->getShop()->getConditionsFilePath(true));
                         $file_attachement['term_of_use']['name'] = "conditions_de_ventes.pdf";
                         $file_attachement['term_of_use']['mime'] = 'application/pdf';
                     }
-                } else {
-                    $file_attachement = null;
-                }
 
+                } else
+                    $file_attachement = null;
+
+                /*  */
                 $emails[] = $result['email'];
                 $emails[] = Configuration::get('PS_SHOP_EMAIL');
 
+                /* Boucle pour l'envoi des emails pour chaque mail renseigne */
                 foreach($emails as $email)
-                    if(!Mail::Send((int)$order->id_lang, $result['template'], $topic, $data, $email, $result['firstname'].' '.$result['lastname'], null, null, $file_attachement, null, _PS_MAIL_DIR_, false, (int)$order->id_shop))
+                    if(!Mail::Send((int)$order->id_lang, $result['template'], $topic, $data, $email, $result['firstname'].' '.$result['lastname'],
+                        null, null, $file_attachement, null, _PS_MAIL_DIR_, false, (int)$order->id_shop))
                         return false;
             }
 
