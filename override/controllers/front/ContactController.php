fdd672737d6be0e0f2eb134ed904396838efe0b9
Thierry <thierry.gozdzicki@provost.fr>
2020-09-04 09:16:19 +0200

    [Front Contact] Envoi de l'email de formulaire de contact

--- override/controllers/front/ContactController.php
+++ override/controllers/front/ContactController.php
@@ -9,7 +9,7 @@ class ContactController extends ContactControllerCore {
 			foreach(array('lastname', 'firstname', 'company', 'number', 'phone', 'email', 'city', 'message') as $name)
 				$templateVars["{$name}"] = $form[$name] ?? '-';
 
-			if(Mail::send(1, 'contact', "Contact client", $templateVars, Configuration::get('PS_SHOP_EMAIL'), Configuration::get('PS_SHOP_NAME'), $form['email'], trim($form['firstname'].' '.$form['lastname'])))
+			if(Mail::send(1, 'contact', "Contact client", $templateVars, Configuration::get('PS_SHOP_EMAIL'), Configuration::get('PS_SHOP_NAME'), Configuration::get('PS_SHOP_EMAIL'), trim($form['firstname'].' '.$form['lastname'])))
 				$this->context->smarty->assign('alert', array('type'=>'success', 'message'=>"Votre message a bien été envoyé."));
 			else
 				$this->context->smarty->assign('alert', array('type'=>'danger', 'message'=>"Désolé, une erreur est survenue pendant l'envoi de votre message."));
