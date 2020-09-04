720ff354a572b96c806c8b59a77885fb9b1b32e8
Thierry <thierry.gozdzicki@provost.fr>
2020-09-04 11:01:03 +0200

    [Front contact] Suppression accolade email

--- override/controllers/front/ContactController.php
+++ override/controllers/front/ContactController.php
@@ -7,7 +7,7 @@ class ContactController extends ContactControllerCore {
 		if($form = Tools::getValue('contact')) {
 
 			foreach(array('lastname', 'firstname', 'company', 'number', 'phone', 'email', 'city', 'message') as $name)
-				$templateVars["{$name}"] = $form[$name] ?? '-';
+				$templateVars['{'.$name.'}'] = $form[$name] ?? '-';
 
 			if(Mail::send(1, 'contact', "Contact client", $templateVars, Configuration::get('PS_SHOP_EMAIL'), Configuration::get('PS_SHOP_NAME'), Configuration::get('PS_SHOP_EMAIL'), trim($form['firstname'].' '.$form['lastname'])))
 				$this->context->smarty->assign('alert', array('type'=>'success', 'message'=>"Votre message a bien été envoyé."));
