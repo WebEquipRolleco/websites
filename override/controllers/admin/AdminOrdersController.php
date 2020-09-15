77078b0085b14c92c95466bcf1eb360fb2fad62b
Damien <damien.collado@provost.fr>
2020-09-15 10:27:25 +0200

    [order] AJout du numéro de devis de dans le mail

--- override/controllers/admin/AdminOrdersController.php
+++ override/controllers/admin/AdminOrdersController.php
@@ -440,6 +440,7 @@ class AdminOrdersController extends AdminOrdersControllerCore {
         $data['{firstname}'] = $this->getCurrentOrder()->getCustomer()->firstname;
         $data['{lastname}'] = $this->getCurrentOrder()->getCustomer()->lastname;
         $data['{shop_phone'] = Configuration::getForOrder('PS_SHOP_PHONE', $this->getCurrentOrder());
+        $data['{devis}'] = $this->$order.getQuotation() ? " via le devis : " + $this->$order.getQuotation()  : ".";
 
         // Proforma
         if($this->getCurrentOrder()->isProforma()) {
@@ -874,4 +875,6 @@ class AdminOrdersController extends AdminOrdersControllerCore {
         $this->content = json_encode($to_return);
         die($this->content);
     }
+
+    
 }
