2e45412f01f96a302cd215bcf22c191f2f8e14d2
Damien <damien.collado@provost.fr>
2020-09-29 16:15:00 +0200

    [facture] test

--- classes/pdf/PDF.php
+++ classes/pdf/PDF.php
@@ -104,13 +104,14 @@ class PDFCore
      */
     public function render($display = true)
     {
+        var_dump("test");
+        die();
         $render = false;
         $this->pdf_renderer->setFontForLang(Context::getContext()->language->iso_code);
         foreach ($this->objects as $object) {
             $this->pdf_renderer->startPageGroup();
             $template = $this->getTemplateObject($object);
-            var_dump("test");
-            die();
+
             if (!$template) {
                 continue;
             }
