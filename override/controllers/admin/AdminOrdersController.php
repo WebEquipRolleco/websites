f119bc7397ea1b72cb09aaf55c8ed61908c06d8a
Damien <damien.collado@provost.fr>
2020-10-02 08:25:47 +0200

    [order] mail de confirmattion

--- override/controllers/admin/AdminOrdersController.php
+++ override/controllers/admin/AdminOrdersController.php
@@ -449,7 +449,7 @@ class AdminOrdersController extends AdminOrdersControllerCore {
         $data['{firstname}'] = $this->getCurrentOrder()->getCustomer()->firstname;
         $data['{lastname}'] = $this->getCurrentOrder()->getCustomer()->lastname;
         $data['{shop_phone'] = Configuration::getForOrder('PS_SHOP_PHONE', $this->getCurrentOrder());
-        $data['{date_payment}'] = $this->getCurrentOrder()->invoice_date->format('d/m/y');
+        $data['{date_payment}'] = $this->getCurrentOrder()->invoice_date;
 
         // Proforma
         if($this->getCurrentOrder()->isProforma()) {
