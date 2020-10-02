1b1861010091f207c9f9703a5d6ca1ae2a997847
Damien <damien.collado@provost.fr>
2020-10-02 08:51:27 +0200

    [order] mail de confirmattion

--- override/classes/order/OrderHistory.php
+++ override/classes/order/OrderHistory.php
@@ -70,7 +70,7 @@ class OrderHistory extends OrderHistoryCore {
 
             $date = DateTime::createFromFormat('Y-m-d H:i:s', $order->date_add);
 
-            if(!$order->invoice_date || empty($order->invoice_date)){
+            if(!$order->invoice_date || empty($order->invoice_date) || date_diff($order->invoice_date, 0 )){
                 $order->invoice_date = date("d/m/Y");
             }
             var_dump($order->invoice_date);
