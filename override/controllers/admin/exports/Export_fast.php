3ee00c93cc7704951f674eeb067090e3ef09f85e
Christian Camelot <christian.camelot@provost.fr>
2020-11-02 15:08:00 +0100

    [export] export simplifié

--- override/controllers/admin/exports/Export_fast.php
+++ override/controllers/admin/exports/Export_fast.php
@@ -17,7 +17,10 @@ class Export_fast extends Export {
         $header[] = "Taux de marge";
         $header[] = "Num client Web";
         $header[] = "Limite de paiement";
+        $header[] = "Num de facture";
         $header[] = "Num client M3";
+        $header[] = "Siret";
+        $header[] = "Nom de société";
         $header[] = "Client";
         $header[] = "Type";
         $header[] = "Mode de paiement";
@@ -38,28 +41,46 @@ class Export_fast extends Export {
 
         foreach(Order::findIds($options) as $id) {
             $order = new Order($id);
-
-            $new_order = true;
-            $total = 0;
-                if ($order->total_discounts > 0) {
-                    continue;
+            foreach($order->getDetails() as $detail) {
+                if ($detail->product_price < 0) {
+                    $detail->total_price_tax_excl = $detail->product_price / 1.2;
                 }
-                $data = array();
-                $data[] = $order->getDate('date_add')->format('d/m/Y');
-                $data[] = $order->reference;
-                $data[] = round($order->getTotalPrice(), 2);
-                $data[] = round($order->getBuyingPrice(), 2);
-                $data[] = round($order->getTotalPrice() - $order->getBuyingPrice(), 2);
-                $data[] = round((($order->getTotalPrice() - $order->getBuyingPrice()) / $order->getTotalPrice()) * 100, 2);
-                $data[] = $order->getCustomer()->id;
-                $data[] = $order->getPaymentDeadline()->format('d/m/Y') == '14/01/0000' ? "" : $order->getPaymentDeadline()->format('d/m/Y');
-                $data[] = $order->getCustomer()->reference;
-                $data[] = $order->getCustomer()->firstname." ".$order->getCustomer()->lastname;
-                $data[] = $order->getCustomer()->getType() ? $order->getCustomer()->getType()->name : '-';
-                $data[] = $order->payment;
-                $data[] = $order->getState()->name;
-                $csv .= implode($this->separator, $data).parent::END_OF_LINE;
+                $total += $detail->total_price_tax_excl;
+                $buying_price = $detail->getTotalBuyingPrice();
+
+                if ($detail->total_price_tax_excl == 0) {
+                    $margin = 0;
+                } else if ($detail->total_price_tax_excl < 0) {
+                    $margin = $detail->total_price_tax_excl;
+                } else {
+                    $margin = $detail->total_price_tax_excl - $buying_price;
+                }
             }
+            if ($order->total_discounts > 0) {
+                continue;
+            }
+            if ($order->invoice_number == 0) {
+                $order->invoice_number = "";
+            }
+            $data = array();
+            $data[] = $order->getDate('date_add')->format('d/m/Y');
+            $data[] = $order->reference;
+            $data[] = round($order->getTotalPrice(), 2);
+            $data[] = round($buying_price, 2);
+            $data[] = round($margin, 2);
+            $data[] = round((($order->getTotalPrice() - $buying_price)  / $order->getTotalPrice()) * 100, 2);
+            $data[] = $order->getCustomer()->id;
+            $data[] = $order->getPaymentDeadline()->format('d/m/Y') == '14/01/0000' ? "" : $order->getPaymentDeadline()->format('d/m/Y');
+            $data[] = $order->invoice_number;
+            $data[] = $order->getCustomer()->reference;
+            $data[] = $order->getCustomer()->siret;
+            $data[] = $order->getCustomer()->company;
+            $data[] = $order->getCustomer()->firstname." ".$order->getCustomer()->lastname;
+            $data[] = $order->getCustomer()->getType() ? $order->getCustomer()->getType()->name : '-';
+            $data[] = $order->payment;
+            $data[] = $order->getState()->name;
+            $csv .= implode($this->separator, $data).parent::END_OF_LINE;
+        }
         $this->RenderCSV("commandes_".date('d-m_H-i').".csv", $csv);
     }
 
