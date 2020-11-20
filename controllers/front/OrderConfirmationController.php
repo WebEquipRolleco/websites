ca9dad2f333da213c69bdd7b6bf7661802f9a789
Damien <damien.collado@provost.fr>
2020-11-20 14:31:31 +0100

    [analytique] test

--- controllers/front/OrderConfirmationController.php
+++ controllers/front/OrderConfirmationController.php
@@ -72,12 +72,10 @@ class OrderConfirmationControllerCore extends FrontController
         $this->order_presenter = new OrderPresenter();
 
         $cart = new Cart((int)$this->id_cart);
-        $this->context->smarty->assign(array(
-            'panier' => array(
-                'cart' => $cart
-            ),
-            'test' => 'hello world'
+        Media::addJsDef('panier', array(
+            'cart' => $cart
         ));
+        Media::addJsDef('toto', "test");
     }
 
     /**
